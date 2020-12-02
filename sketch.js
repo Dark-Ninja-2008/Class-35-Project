a63042c1fa122e550ecb4b1dd94dee5a94205c88
Dark-Ninja-2008 <71764715+Dark-Ninja-2008@users.noreply.github.com>
2020-12-02 13:52:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,53 @@
+var ball;
+var database,position;
+
+function setup(){
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    //firebase.database(database);
+    database = firebase.database();
+    position = database.ref("ball/pos");
+    position.on("value",readposition,showerror);
+
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        changePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        changePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        changePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        changePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function changePosition(x,y){
+    database.ref("ball/pos").set({
+      x:ball.x+x,
+      y:ball.y+y
+    })
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+    
+}
+
+function readposition(data){
+   position=data.val(); 
+   ball.x=position.x;
+   ball.y=position.y;
+   console.log(position);
+}
+
+function showerror(){
+    console.log();
+}
+
